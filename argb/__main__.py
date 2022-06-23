01dc861769ad4defab8b1b03749648bf88393528
Andrew Thompson <6337878+Mrwerdo@users.noreply.github.com>
2022-06-23 14:26:45 +1000

    Added instructions for installing dependencies and uploading to board locally

--- argb/__main__.py
+++ argb/__main__.py
@@ -2,7 +2,7 @@ print('ARGB Controller Started...')
 from .coms import *
 from .server import Server
 
-server = Server('/dev/cu.usbmodem144201')
+server = Server('/dev/ttyACM0')
 
 
 def light(**kwargs):
@@ -12,10 +12,13 @@ def light(**kwargs):
 
 with server.connection:
     sleep(1)
-    print(server.connection.receiveMessage())
-    light(index=0, start=0, end=20, start_color=(255, 255, 255), end_color=(0, 0, 255), ahds=(0, 0, 0, 50))
-    # for x in range(0, 20):
-        # print(f'trying index: {x}')
-        # light(index=0, start=x, end=x+1, start_color=(255, 0, 0), end_color=(0, 0, 255), ahds=(50, 10, 50, 10))
-        # sleep(2)
-        # light(index=0, start=x, end=x+1, start_color=(0, 0, 0), end_color=(0, 0, 0), ahds=(0, 0, 0, 0))
+    if server.connection.serialPort.in_waiting != 0:
+        print(server.connection.receiveMessage())
+    light(index=0, start=0, end=6, start_color=(255, 255, 255), end_color=(0, 0, 255), ahds=(5, 5, 5, 5))
+    light(index=1, start=6, end=12, start_color=(255, 0, 0), start_color_alt=(0, 255, 0), end_color=(0, 0, 255), end_color_alt=(0, 255, 255), ahds=(5, 5, 5, 5))
+    light(index=2, start=12, end=20, start_color=(255, 0, 0), start_color_alt=(255, 255, 0), end_color=(0, 255, 255), end_color_alt=(0, 0, 255), ahds=(5, 5, 5, 5))
+#    for x in range(0, 20):
+#        print(f'trying index: {x}')
+#        light(index=0, start=x, end=x+1, start_color=(255, 0, 0), end_color=(0, 0, 255), ahds=(50, 10, 50, 10))
+#        sleep(2)
+#        light(index=0, start=x, end=x+1, start_color=(0, 0, 0), end_color=(0, 0, 0), ahds=(0, 0, 0, 0))
--- readme.md
+++ readme.md
@@ -1,14 +1,47 @@
 # ARGB Controller
 
-To install nanopb:
+Install arduino-cli or use the IDE.
 
-    pip install nanopb
+To install the board firmware:
+
+    arduino-cli core install arduino:avr
+
+To install python dependencies:
+
+    pip install -r requirements.txt
+
+To activate env:
+
+    source .venv/bin/activate
+
+To install FastLED:
+
+    arduino-cli lib install "FastLED"
+    arduino-cli lib install SnappyProto
+    arduino-cli lib install PacketSerial
+    arduino-cli lib install AceCRC
 
 To build protobuf messages:
 ```
 $ protoc --python-out=. messages.proto
 $ nanopb_generator messages.proto
 ```
+
+To compile the sketch:
+
+    arduino-cli compile --fqbn arduino:avr:uno argb_controller
+
+To upload the sketch:
+
+    arduino-cli upload --port /dev/ttyACM0 --fqbn arduino:avr:uno argb_controller.ino
+
+To ensure the device is not reset by the operating system:
+
+    stty -F /dev/ttyACM0 -hupcl
+
+To run the python program:
+
+    python -m argb
 
 Dependencies:
 - [AceCRC](https://github.com/bxparks/AceCRC)
